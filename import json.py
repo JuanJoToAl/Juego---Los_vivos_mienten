8ca40187d35b11758eb1f43fd1d90cf304ad4323
JuanJoToAl <jutobara@unal.edu.co>
2024-09-18 13:25:59 -0500

    borrando archivos

--- import json.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('dialogos.json', 'r', encoding='utf-8') as f:
-    data = json.load(f)
-
-for escena, contenido in data.items():
-    print(contenido["dialogo"])
-    for opcion in contenido["opciones"]:
-        print(opcion)
